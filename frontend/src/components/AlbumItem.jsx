1f2d323a1f70c9de691ca4495e3a44f2aa794dc8
Rutansh chawla <rutansh3456@gmail.com>
2025-05-16 14:29:36 +0530

    bugs fixed

--- frontend/src/components/AlbumItem.jsx
+++ frontend/src/components/AlbumItem.jsx
@@ -6,7 +6,7 @@ function AlbumItem({image, name, desc, id}) {
   const navigate = useNavigate();
 
   return (
-    <div onClick={()=>{navigate(`/album/${id}`)}} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+    <div onClick={()=>{navigate(`/album/${id}`)}} className='min-w-[180px] h-10/12 w-2/8 p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
       <img src={image} alt='image' className='rounded'/>
       <p className='font-bold mt-2 mb-1'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
